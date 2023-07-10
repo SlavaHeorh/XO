750ee57c16166c5733ccc774f3a10818f24bc969
SlavaHeorh <slava.georg.k@gmail.com>
2023-07-10 13:41:41 +0300

    test

---
+++ components/games/game-cell.jsx
@@ -0,0 +1,13 @@
+import { GameSymbol } from '../games/game-symbol'
+import styles from '../games/game.module.css'
+
+export function GameCell({ isWinner, onClick, symbol }) {
+  return (
+    <button
+      className={`${styles['cell']} ${isWinner ? styles['cell--win'] : ''}`}
+      onClick={onClick}
+    >
+      {symbol ? <GameSymbol symbol={symbol} /> : null}
+    </button>
+  )
+}
---
+++ components/games/game-info.jsx
@@ -0,0 +1,26 @@
+import { GameSymbol } from "../games/game-symbol"
+import styles from '../games/game.module.css'
+
+export function GameInfo({ isDraw, winnerSymbol, currentStep }) {
+  if (isDraw) {
+    return (
+      <div className={styles["game-info"]}>
+        Ничья
+      </div>
+    )
+  }
+
+  if (winnerSymbol) {
+    return (
+      <div className={styles["game-info"]}>
+        Победитель: <GameSymbol symbol={winnerSymbol} />
+      </div>
+    )
+  }
+
+  return (
+    <div className={styles["game-info"]}>
+      Ход: <GameSymbol symbol={currentStep} />
+    </div>
+  )
+}
