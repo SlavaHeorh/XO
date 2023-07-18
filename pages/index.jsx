f11e65ab1e48dfbc5e5e533b01c93b83689fb887
SlavaHeorh <slava.georg.k@gmail.com>
2023-07-18 15:46:01 +0300

    added modal window

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,8 @@ import {Header} from "../components/header";
 import {GameInfo, GameTitle, GameField, useGameState} from "../components/game";
 import {useState} from "react";
 import {GameSymbol} from "../components/game/game-symbol";
+import {UiModal} from "../components/uikit/ui-modal";
+import {UiButton} from "../components/uikit/ui-button";
 
 
 export default function HomePage() {
@@ -26,7 +28,16 @@ export default function HomePage() {
                         <GameSymbol symbol={winnerSymbol}/>
                     </div>
                 )}
-
+                <UiModal width="md">
+                    <UiModal.Header>Game over</UiModal.Header>
+                    <UiModal.Body>
+                        <div className="text-sm  ">Winner: <span className="text-teal-600">Player One</span></div>
+                    </UiModal.Body>
+                    <UiModal.Footer>
+                        <UiButton size="md" variant="outline">Back</UiButton>
+                        <UiButton size="md" variant="primary">Play again</UiButton>
+                    </UiModal.Footer>
+                </UiModal>
                 <GameField
                     className="mt-6"
                     cells={cells}
